50f3dc3fb1de466921a331c1fa10d1a3691cbac0
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-07-14 14:46:45 +0200

    simpel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function Form({ status = "error" }) {
+  if (status === "success") {
+    return <h1>That's right!</h1>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form>
+        <textarea />
+        <br />
+        <button>Submit</button>
+      </form>
+    </>
   );
 }
-
-export default App;
