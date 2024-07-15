f2f4fd1e0e2e13a5f6a7b67ea56a1adc8cc189aa
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-07-15 10:43:22 +0200

    stopped as info is missing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-export default function Form({
-  // Try 'submitting', 'error', 'success':
-  status = "submitting",
-}) {
-  if (status === "success") {
-    return <h1>That's right!</h1>;
+import { useState } from "react";
+import AddTask from "./AddTask.js";
+import TaskList from "./TaskList.js";
+
+export default function TaskApp() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  function handleAddTask(text) {
+    setTasks([
+      ...tasks,
+      {
+        id: nextId++,
+        text: text,
+        done: false,
+      },
+    ]);
   }
+
+  function handleChangeTask(task) {
+    setTasks(
+      tasks.map((t) => {
+        if (t.id === task.id) {
+          return task;
+        } else {
+          return t;
+        }
+      })
+    );
+  }
+
+  function handleDeleteTask(taskId) {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  }
+
   return (
     <>
-      <h2>City quiz</h2>
-      <p>
-        In which city is there a billboard that turns air into drinkable water?
-      </p>
-      <form>
-        <textarea disabled={status === "submitting"} />
-        <br />
-        <button disabled={status === "empty" || status === "submitting"}>
-          Submit
-        </button>
-        {status === "error" && (
-          <p className="Error">Good guess but a wrong answer. Try again!</p>
-        )}
-      </form>
+      <h1>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
     </>
   );
 }
+
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: "Visit Kafka Museum", done: true },
+  { id: 1, text: "Watch a puppet show", done: false },
+  { id: 2, text: "Lennon Wall pic", done: false },
+];
