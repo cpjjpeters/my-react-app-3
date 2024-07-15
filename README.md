24e35454526f36fe0688c6718c500c239f8fe431
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-07-15 11:22:47 +0200

    scaling up

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ React demo
 https://react.dev/learn/extracting-state-logic-into-a-reducer
 
 But not working as AddTask.js and TaskList.js are empty now
+
+So using it for https://react.dev/learn/scaling-up-with-reducer-and-context
--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import AddTask from "./AddTask.js";
 import TaskList from "./TaskList.js";
 
 export default function TaskApp() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
 
   function handleAddTask(text) {
-    setTasks([
-      ...tasks,
-      {
-        id: nextId++,
-        text: text,
-        done: false,
-      },
-    ]);
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
   }
 
   function handleChangeTask(task) {
-    setTasks(
-      tasks.map((t) => {
-        if (t.id === task.id) {
-          return task;
-        } else {
-          return t;
-        }
-      })
-    );
+    dispatch({
+      type: "changed",
+      task: task,
+    });
   }
 
   function handleDeleteTask(taskId) {
-    setTasks(tasks.filter((t) => t.id !== taskId));
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
   }
 
   return (
     <>
-      <h1>Prague itinerary</h1>
+      <h1>Day off in Kyoto</h1>
       <AddTask onAddTask={handleAddTask} />
       <TaskList
         tasks={tasks}
@@ -45,9 +40,93 @@ export default function TaskApp() {
   );
 }
 
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+}
+
 let nextId = 3;
 const initialTasks = [
-  { id: 0, text: "Visit Kafka Museum", done: true },
-  { id: 1, text: "Watch a puppet show", done: false },
-  { id: 2, text: "Lennon Wall pic", done: false },
+  { id: 0, text: "Philosopher’s Path", done: true },
+  { id: 1, text: "Visit the temple", done: false },
+  { id: 2, text: "Drink matcha", done: false },
 ];
+
+// import { useState } from "react";
+// import AddTask from "./AddTask.js";
+// import TaskList from "./TaskList.js";
+
+// export default function TaskApp() {
+//   const [tasks, setTasks] = useState(initialTasks);
+
+//   function handleAddTask(text) {
+//     setTasks([
+//       ...tasks,
+//       {
+//         id: nextId++,
+//         text: text,
+//         done: false,
+//       },
+//     ]);
+//   }
+
+//   function handleChangeTask(task) {
+//     setTasks(
+//       tasks.map((t) => {
+//         if (t.id === task.id) {
+//           return task;
+//         } else {
+//           return t;
+//         }
+//       })
+//     );
+//   }
+
+//   function handleDeleteTask(taskId) {
+//     setTasks(tasks.filter((t) => t.id !== taskId));
+//   }
+
+//   return (
+//     <>
+//       <h1>Prague itinerary</h1>
+//       <AddTask onAddTask={handleAddTask} />
+//       <TaskList
+//         tasks={tasks}
+//         onChangeTask={handleChangeTask}
+//         onDeleteTask={handleDeleteTask}
+//       />
+//     </>
+//   );
+// }
+
+// let nextId = 3;
+// const initialTasks = [
+//   { id: 0, text: "Visit Kafka Museum", done: true },
+//   { id: 1, text: "Watch a puppet show", done: false },
+//   { id: 2, text: "Lennon Wall pic", done: false },
+// ];
